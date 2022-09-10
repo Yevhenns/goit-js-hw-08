09d30549fe522e1cfa9995cb6b912ee633918f04
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-10 17:06:49 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,17 +4,17 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
-const durationData = {
-  videoPlayerCurrentTime: 45,
-};
-player.on('timeupdate', function (duration) {
-  durationData.videoPlayerCurrentTime = player.on;
-  console.log(durationData);
-});
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(timeupdate) {
+  let pauseTime = timeupdate.seconds;
+  console.log(pauseTime);
+}
