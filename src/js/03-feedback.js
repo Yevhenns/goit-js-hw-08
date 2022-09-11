4d2d277a110f88ba2e23d8fa39fef3a0a87c0f8e
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-11 15:52:44 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+console.log(form);
+const formData = localStorage.getItem('feedback-form-state');
+
+form.addEventListener('input', event => {});
