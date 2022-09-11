f38a3935ccf872125a5d04db028515e659628a9f
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-11 10:28:29 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,13 +8,32 @@ const player = new Player(iframe);
 //   console.log('played the video!');
 // });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+console.log(savedTime);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay(timeupdate) {
-  let pauseTime = timeupdate.seconds;
-  console.log(pauseTime);
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 }
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
